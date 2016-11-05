de1b4b6d9a4a7943cfd4592c6f4f9653b67c51fb
rtfgvb74125 <105021056@live.asia.edu.tw>
2016-11-05 16:32:51 +0800

    105021056week 06 HW繳交

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,36 @@
 package hw;
 
+/*
+ * Topic: 請設計一個函數fun(v1, type)，
+ * 讓使用者輸入一個溫度值及計算方式(type 為1 時 華氏->攝氏，type為2時攝氏->華氏)，
+ * 函數算出結果後要回傳至主程式再印出結果。(F=C*9/5+32), (C = (F-32) * (5/9)，
+ * 例如輸入100 1 則輸出 37.77778; 輸入 37.77778 2 時則輸出 100.0
+ * Date: 2016/10/31
+ * Author: 105021056 王家恩
+ */
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入溫度 ");
+		float n = scn.nextFloat();
+		System.out.println("請輸入1或2 :");
+		float m = scn.nextFloat();
+		System.out.println("溫度轉變為 = " + fun(n, m));
 	}
 
+	public static float fun(float n, float m) {
+		if (m == 1) {
+			n = (float) (n * 9 / 5 + 32);
+		} else if (m == 2) {
+			n = (float) (n - 32) * ((float) 5 / 9);
+		} else {
+			System.out.println("輸入錯誤訊息溫度無法轉變");
+		}
+		return (float) n;
+
+	}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,37 @@
 package hw;
 
+/*
+ * Topic:讓使用者輸入性別與身高，透過函數呼叫，
+ * 依據男女不同, 幫她(他)計算並輸出其標準體重 
+ * ((1)男：(身高-170)*0.6+62 , (2)女：(身高-158)*0.5+52)。
+ * 函數算出結果後要回傳至主程式再印出結果。例如:輸入 1 170 則輸出 62.0, 輸入 2 165 則輸出 55.5 
+ * Date: 2016/10/31
+ * Author: 105021056 王家恩
+ */
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請選擇1男生或2女生");
+		float n = scn.nextFloat();
+System.out.println("請輸入身高 :");
+float m = scn.nextFloat();		
+System.out.print("標準體重為 :"  + fun(n,m));
+	}
 
+	public static float fun(float n,float m) {
+		Scanner scn = new Scanner(System.in);
+		if(n==1){
+			m = (float)((m-170)*0.6+62);
+		}else if(n==2){
+			m = (float)((m-158)*0.5+52);
+		}else{
+			System.out.print("輸入錯誤指令無法算出標準體重");
+		}
+		return m;
 	}
 
 }
